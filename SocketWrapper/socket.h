c51d48095d7a696e2fb06744dfb33ccfe25cd8ef
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-07-19 23:11:48 +0400

    Update socket.h

--- SocketWrapper/socket.h
+++ SocketWrapper/socket.h
@@ -4,14 +4,14 @@
 #include <string>
 
 #ifdef _WIN32
-    #include <winsock2.h>
-    #pragma comment(lib, "ws2_32.lib")
+    	#include <winsock2.h>
+    	#pragma comment(lib, "ws2_32.lib")
 #else
-    #include <sys/types.h>
-    #include <sys/socket.h>
-    #include <netinet/in.h>
-    #include <arpa/inet.h>
-    #include <unistd.h>
+    	#include <sys/types.h>
+    	#include <sys/socket.h>
+    	#include <netinet/in.h>
+    	#include <arpa/inet.h>
+    	#include <unistd.h>
 #endif
 
 enum class Domain {
@@ -27,33 +27,33 @@ enum class Type {
 class Socket {
 public:
 	Socket(Domain, Type, int);
-    ~Socket();
-    Socket socket();
-    void create();
+    	~Socket();
+    	Socket socket();
+    	void create();
 	void connect(const std::string&, uint16_t);
-    void bind(const std::string&, uint16_t);
-    void listen(int);
-    Socket accept();
-    ssize_t send(const void*, size_t);
-    ssize_t recv(void*, size_t);
-    void close();
-    int getSocketFd() const;
-    Domain getDomain() const;
-    Type getType() const;
-    int getProtocol() const;
-    std::string getLocalIpAddress() const;
-    uint16_t getLocalPort() const;
-    std::string getRemoteIpAddress() const;
-    uint16_t getRemotePort() const;
+    	void bind(const std::string&, uint16_t);
+    	void listen(int);
+    	Socket accept();
+    	ssize_t send(const void*, size_t);
+    	ssize_t recv(void*, size_t);
+    	void close();
+    	int getSocketFd() const;
+    	Domain getDomain() const;
+    	Type getType() const;
+    	int getProtocol() const;
+    	std::string getLocalIpAddress() const;
+    	uint16_t getLocalPort() const;
+    	std::string getRemoteIpAddress() const;
+    	uint16_t getRemotePort() const;
 private:
 	Domain domain;
 	Type type;
 	int protocol;
 	int socketFd;
 	std::string localIpAddress;
-    uint16_t localPort;
-    std::string remoteIpAddress;
-    uint16_t remotePort;
+    	uint16_t localPort;
+    	std::string remoteIpAddress;
+    	uint16_t remotePort;
 };
 
 #endif // SOCKET_H
