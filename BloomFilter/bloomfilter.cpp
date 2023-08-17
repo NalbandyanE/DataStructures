5753a4189eab73f5785d01ab53d3803517c3eeb1
Elmira <89019222+NalbandyanElmira@users.noreply.github.com>
2023-08-17 22:52:39 +0400

    Update bloomfilter.cpp

--- BloomFilter/bloomfilter.cpp
+++ BloomFilter/bloomfilter.cpp
@@ -12,25 +12,25 @@ BloomFilter::BloomFilter(int nC, const std::vector<std::hash<std::string>>& f)
 
 void BloomFilter::add(const std::string& h) {
 	for (auto const& item: hashFunctions) {
-        cells[item(h) % numOfCells] = true;
-    }
+        	cells[item(h) % numOfCells] = true;
+    	}
 }
 
 std::pair<bool, double> BloomFilter::search(const std::string& h, int numElements) {
-    auto f = std::find_if(hashFunctions.begin(), hashFunctions.end(),
-        [this, &h](const std::hash<std::string>& hash) {
-            return !cells[hash(h) % numOfCells];
-        });
+    	auto f = std::find_if(hashFunctions.begin(), hashFunctions.end(),
+        	[this, &h](const std::hash<std::string>& hash) {
+            		return !cells[hash(h) % numOfCells];
+        	});
 
-    double falsePositiveProb = falsePositiveProbability(numElements);
+    	double falsePositiveProb = falsePositiveProbability(numElements);
 
-    return std::make_pair(f == hashFunctions.end(), falsePositiveProb);
+    	return std::make_pair(f == hashFunctions.end(), falsePositiveProb);
 }
 
 double BloomFilter::falsePositiveProbability(int numElements) {
-    double k = static_cast<double>(hashFunctions.size());
-    double m = static_cast<double>(numOfCells);
-    double n = static_cast<double>(numElements);
+    	double k = static_cast<double>(hashFunctions.size());
+    	double m = static_cast<double>(numOfCells);
+    	double n = static_cast<double>(numElements);
 
-    return std::pow(1 - std::exp(-k * n / m), k);
+    	return std::pow(1 - std::exp(-k * n / m), k);
 }
